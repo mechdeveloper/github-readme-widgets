c494d38f90e329cb26d4db5b78d1ae74788b1712
Ashish Singh Baghel <mechashishsingh@gmail.com>
2022-04-18 21:16:33 +0000

    configure to return pure svg response

--- src/cards/badges-card.js
+++ src/cards/badges-card.js
@@ -1,16 +1,40 @@
 const renderBadgesCard = (badges) => {
     // console.log(`badges-card: BEGIN`);
+    const totalCount = badges.metadata.total_count;
+    let transformX = 0;
+    let transformY = 0;
+
+    let badgesCard = '<svg width="100%" height="100%" id="credly-badger-main" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" >'
     
-    let badgesCard = '<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg"><foreignObject width="100%" height="100%"><div align="center" xmlns="http://www.w3.org/1999/xhtml">'
+    // for (let item of badges.data){
+    //     badgesCard += `<g transform="translate(${transformX += 100},0)"><svg>`
+    //     badgesCard += `<defs><clipPath id="${item.id}">`
+    //     badgesCard += `<rect width="100" height="100" />`
+    //     badgesCard += `</clipPath></defs>`
+    //     badgesCard += `<image width="100" height="100" xlink:href="${item.image_url}" clip-path="url(#${item.id})" />`
+    //     badgesCard += `</svg></g>`
+    //     // badgesCard += `<a href="${item.badge_template.url}">`;
+    //     // badgesCard += `<img width="120" height="120" alt="${item.badge_template.name}" src="${item.image_url}" />`
+    //     // badgesCard += '</a>';
+    // }
 
-    for (let item of badges.data){
-        // console.log(item.id);
-        badgesCard += `<a href="${item.badge_template.url}">`;
-        badgesCard += `<img width="120" height="120" alt="${item.badge_template.name}" src="${item.image_url}" />`
-        badgesCard += '</a>';
-    }
+    
+    badges.data.forEach(function (item, i ) {
+        // console.log('%d: %s', i, item.id);
+        badgesCard += `<g transform="translate(${transformX},${transformY})"><svg>`
+        badgesCard += `<defs><clipPath id="${item.id}">`
+        badgesCard += `<rect width="100" height="100" />`
+        badgesCard += `</clipPath></defs>`
+        badgesCard += `<image width="100" height="100" xlink:href="${item.image_url}" clip-path="url(#${item.id})" />`
+        badgesCard += `</svg></g>`
+        // badgesCard += `<a href="${item.badge_template.url}">`;
+        // badgesCard += `<img width="120" height="120" alt="${item.badge_template.name}" src="${item.image_url}" />`
+        // badgesCard += '</a>';
+        transformX += 100;
+    });
 
-    badgesCard += '</div></foreignObject></svg>';
+    // badgesCard += '</div></foreignObject></svg>';
+    badgesCard += '</svg>';
     
     // console.log(`badges-card: ${badgesCard}`);
     // console.log(`badges-card: END`);
