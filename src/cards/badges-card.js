5c13571caa247d72add94a5acfd099422adfac4c
Ashish Singh Baghel <mechashishsingh@gmail.com>
2022-04-19 23:45:04 +0000

    Fix badges card to return pure svg

--- src/cards/badges-card.js
+++ src/cards/badges-card.js
@@ -1,25 +1,79 @@
 const renderBadgesCard = (badges) => {
     // console.log(`badges-card: BEGIN`);
     const totalCount = badges.metadata.total_count;
-    let transformX = 0;
-    let transformY = 0;
 
-    let badgesCard = '<svg width="100%" height="100%" id="credly-badger-main" fill="none" xmlns="http://www.w3.org/2000/svg" >'
+    let certItems = ` `;
+    let certItemPropY = 0;
 
-    badges.data.forEach(function (item, i ) {
-        // console.log('%d: %s', i, item.id);
-        badgesCard += `<g transform="translate(${transformX},${transformY})">`
-        badgesCard += `<image width="100" height="100" href="${item.image_url}?sanitize=true" />`
-        badgesCard += `</g>`
-        // badgesCard += `<a href="${item.badge_template.url}">`;
-        // badgesCard += `<img width="120" height="120" alt="${item.badge_template.name}" src="${item.image_url}" />`
-        // badgesCard += '</a>';
-        transformX += 100;
-    });
+    // badges.data.forEach(function (item, i ) {
+    // for (const item of badges.data) {
+    for (let i = 0; i < 5; i++) {
+        certItems += `<text x="0" y="${certItemPropY}" class="stagger">${badges.data[i].badge_template.name}</text>>`;
+        certItemPropY += 20;   
+    }
 
-    // badgesCard += '</div></foreignObject></svg>';
-    badgesCard += '</svg>';
-    
+    let badgesCard = `
+        <svg version="1.1"
+            width="595" 
+            height="195"
+            viewBox="0 0 595 195"
+            fill="#ffffff"
+            xmlns="http://www.w3.org/2000/svg" 
+            xmlns:xlink="http://www.w3.org/1999/xlink" >
+
+            <rect data-id="card-bg" 
+              x="0.5" 
+              y="0.5" 
+              rx="4.5"
+              width="99%" 
+              height="99%"
+              stroke="#ffffff"
+              fill="#000000"
+              stroke-opacity="1"
+            />
+
+            <g data-id="card-title" 
+              transform="translate(25, 35)">
+
+                <g transform="translate(0, 0)">
+                  <text
+                    x="0"
+                    y="0"
+                    class="header"
+                    data-testid="header">
+                        Exams and Certifications
+                  </text>
+                </g>
+            </g>
+
+            <g data-id="main-card-body"
+              transform="translate(25, 75)">
+            
+              <g data-id="Total Badges" 
+                transform="translate(425, 50)">
+                <text
+                    x="0"
+                    y="0"
+                    font-size="100"
+                    class="stagger">
+                    10
+                </text>
+                <text
+                    x="0"
+                    y="30"
+                    font-size="20"
+                    class="stagger">
+                    Total Badges
+                </text>
+              </g>
+            
+              ${certItems}
+            </g>
+        </svg>
+    `
+
+
+
     // console.log(`badges-card: ${badgesCard}`);
     // console.log(`badges-card: END`);
     return `${badgesCard}`;
